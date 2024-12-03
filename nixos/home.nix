1ba1e5d3b07faf7e1e971e554990c6535bba246b
Michael Lange <dingoeatingfuzz@gmail.com>
2024-12-03 12:54:12 -0800

    Formatting

--- nixos/home.nix
+++ nixos/home.nix
@@ -72,7 +72,7 @@
     settings = {
       window.padding = {
         x = 10;
-	y = 10;
+        y = 10;
       };
       scrolling = {
         history = 5000;
@@ -81,13 +81,13 @@
         size = 12;
         # draw_bold_text_with_bright_colors = true;
         normal = {
-	  family = "SauceCodePro Nerd Font";
-	  style = "Regular";
-	};
-        bold = {
-	  family = "SauceCodePro Nerd Font";
-	  style = "Bold";
-	};
+          family = "SauceCodePro Nerd Font";
+          style = "Regular";
+          };
+          bold = {
+          family = "SauceCodePro Nerd Font";
+          style = "Bold";
+        };
       };
     };
   };
