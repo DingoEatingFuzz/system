907fc44de30be2ddecedd5d4912c09cbdc770c8c
Michael Lange <dingoeatingfuzz@gmail.com>
2024-12-04 23:47:23 -0800

    Formatting, mostly

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -10,39 +10,47 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nvim-wrapper = {
-      url = "git+file:/neovim2?shallow=1";
+      url = "path:../neovim2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, nixos-hardware, home-manager, ... }: {
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem rec {
-        system = "x86_64-linux";
-        specialArgs = {
-          pkgs-unstable = import nixpkgs-unstable {
-            inherit system;
-            config.allowUnfree = true;
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      nixos-hardware,
+      home-manager,
+      nvim-wrapper,
+      ...
+    }:
+    {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem rec {
+          system = "x86_64-linux";
+          specialArgs = {
+            pkgs-unstable = import nixpkgs-unstable {
+              inherit system;
+              config.allowUnfree = true;
+            };
+            nvim-wrapper = import nvim-wrapper { inherit system; };
           };
-          nvim-wrapper = import nvim-wrapper {
-            inherit system;
-          };
+          modules = [
+            nixos-hardware.nixosModules.framework-intel-core-ultra-series1
+            ./configuration.nix
+            ./hardware-configuration.nix
+            ./fonts.nix
+            ./neovim/neovim.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = specialArgs;
+              home-manager.users.michael = import ./home.nix;
+            }
+          ];
         };
-        modules = [
-          nixos-hardware.nixosModules.framework-intel-core-ultra-series1
-          ./configuration.nix
-          ./hardware-configuration.nix
-          ./fonts.nix
-          ./neovim/neovim.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = specialArgs;
-            home-manager.users.michael = import ./home.nix;
-          }
-        ];
       };
     };
-  };
 }
