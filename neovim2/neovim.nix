eedd92838f6d55e6962fd1974e41c39201bc060a
Michael Lange <dingoeatingfuzz@gmail.com>
2024-12-04 19:47:41 -0800

    Neovim package...just have to integrate it somehow now...

---
+++ neovim2/neovim.nix
@@ -0,0 +1,51 @@
+{
+  symlinkJoin,
+  neovim-unwrapped,
+  makeWrapper,
+  runCommandLocal,
+  vimPlugins,
+  lib
+}:
+let
+  packageName = "nvim2";
+
+  startPlugins = [
+    vimPlugins.telescope-nvim
+    vimPlugins.nvim-treesitter.withAllGrammars
+  ];
+
+  foldPlugins = builtins.foldl' (
+    acc: next:
+      acc ++ [ next ] ++ (foldPlugins (next.dependencies or []))
+  ) [];
+
+  startPluginsWithDeps = lib.unique (foldPlugins startPlugins);
+
+  packpath = runCommandLocal "packpath" {} ''
+    mkdir -p $out/pack/${packageName}/{start,opt}
+
+    ${
+      lib.concatMapStringsSep
+      "\n"
+      (plugin: "ln -vsfT ${plugin} $out/pack/${packageName}/start/${lib.getName plugin}")
+      startPluginsWithDeps
+    }
+  '';
+in
+  symlinkJoin {
+    name = "neovim-custom";
+    paths = [neovim-unwrapped];
+    nativeBuildInputs = [makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/nvim \
+        --add-flags '-u' \
+        --add-flags '${./init.lua}' \
+        --add-flags '--cmd' \
+        --add-flags "'set packpath^=${packpath} | set runtimepath^=${packpath}'" \
+        --set-default NVIM_APPNAME nvim2
+    '';
+
+    passthru = {
+      inherit packpath;
+    };
+  }
