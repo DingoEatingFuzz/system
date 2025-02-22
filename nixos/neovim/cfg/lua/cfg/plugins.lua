859c9298d827835c614647a29be401f9bf75ee86
Michael Lange <dingoeatingfuzz@gmail.com>
2025-02-22 08:07:48 -0800

    Telescope config

--- nixos/neovim/cfg/lua/cfg/plugins.lua
+++ nixos/neovim/cfg/lua/cfg/plugins.lua
@@ -9,6 +9,9 @@ vim.opt.termguicolors = true
 -- File tree viewer
 require("nvim-tree").setup(require("cfg.configs.nvimtree"))
 
+-- Telescope / Fuzzy find
+require("telescope").setup(require("cfg.configs.telescope"))
+
 -- setup language servers
 local lspconfig = require("lspconfig")
 local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
