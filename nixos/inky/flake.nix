133805ecd11635da48e66786fab10d1c5343ae53
Michael Lange <dingoeatingfuzz@gmail.com>
2025-06-17 15:27:20 -0700

    Inky (working)

--- nixos/inky/flake.nix
+++ nixos/inky/flake.nix
@@ -3,19 +3,24 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, ... }@inputs: {
+    packages.x86_64-linux =
     let
-      inherit nixpkgs;
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config = { allowUnfree = true; };
+      };
+
       pname = "inky";
       version = "0.15.1";
       description = "An editor for the ink interactive narrative markup language";
 
       desktopItem = let
-        icon = nixpkgs.fetchurl {
+        icon = pkgs.fetchurl {
           url = "https://raw.githubusercontent.com/inkle/inky/${version}/resources/Icon1024.png";
           hash = "sha256-82+s7MZ8a/GGPeNVZMlbC7n1IVpgqSO/xPYrtXaEIOs=";
         };
-      in nixpkgs.makeDesktopItem {
+      in pkgs.makeDesktopItem {
         name = "Inky";
         exec = "inky";
         icon = icon;
@@ -25,11 +30,12 @@
       };
 
       inkyPkg = let 
-        src = nixpkgs.fetchzip {
+        src = pkgs.fetchzip {
           url = "https://github.com/inkle/inky/releases/download/${version}/Inky_linux.zip";
-          hash = "sha256-WK63bmW6n/hsrjhBzDAi1EjxPiP2TxtuaxAIa84TAsk=";
+          hash = "sha256-Ak4MQRb20R2VaEiXP214bvswe1DRvf6caPCdEw5FEKc=";
+          stripRoot = false;
         };
-      in nixpkgs.stdenv.mkDerivation {
+      in pkgs.stdenv.mkDerivation {
         inherit pname version src;
 
         dontConfigure = true;
@@ -47,70 +53,72 @@
           runHook postInstall
         '';
       };
-    in 
-      let
-        inherit nixpkgs;
-        inky = nixpkgs.buildFHSUserEnv {
-          inherit pname version;
-          targetPkgs = pkgs: with pkgs; [
-            inkyPkg
-            udev
 
-            # compiler packages
-            icu
-            zlib
+      inky = pkgs.buildFHSEnv {
+        inherit pname version inkyPkg;
+        targetPkgs = pkgs: with pkgs; [
+          inkyPkg
+          udev
 
-            # electron packages
-            alsa-lib
-            atk
-            cairo
-            cups
-            dbus
-            expat
-            gdk-pixbuf
-            glib
-            gtk3
-            libuuid
-            nspr
-            nss
-            pango
-            xorg.libX11
-            xorg.libXScrnSaver
-            xorg.libXcomposite
-            xorg.libXcursor
-            xorg.libXdamage
-            xorg.libXext
-            xorg.libXfixes
-            xorg.libXi
-            xorg.libXrandr
-            xorg.libXrender
-            xorg.libXtst
-            xorg.libxcb
-          ];
+          # compiler packages
+          icu
+          zlib
 
-          runScript = "Inky $*";
+          # electron packages
+          alsa-lib
+          atk
+          cairo
+          cups
+          dbus
+          expat
+          gdk-pixbuf
+          glib
+          gtk3
+          libuuid
+          libdrm
+          libgbm
+          libxkbcommon
+          mesa
+          nspr
+          nss
+          pango
+          xorg.libX11
+          xorg.libXScrnSaver
+          xorg.libXcomposite
+          xorg.libXcursor
+          xorg.libXdamage
+          xorg.libXext
+          xorg.libXfixes
+          xorg.libXi
+          xorg.libXrandr
+          xorg.libXrender
+          xorg.libXtst
+          xorg.libxcb
+        ];
 
-          extraInstallCommands = ''
-             mkdir -p "$out/share/applications"
-             ln -s ${desktopItem}/share/applications/* "$out/share/applications"
-           '';
+        runScript = "Inky $*";
 
-          meta = {
-            inherit description;
-            longDescription = ''
-              Inky is an editor for ink, inkle's markup language for writing interactive narrative in games, as used in "80 Days".
-              It's an IDE (integrated development environment), because it gives you a single app that lets you play in the editor as you write, and fix any bugs in your code.
-            '';
-            mainProgram = "inky";
-            homepage = "https://github.com/inkle/inky";
-            license = nixpkgs.lib.licenses.mit;
-            platforms = [ "x86_64-linux" ];
-          };
-        };
-      in {
-        packages.x86_64-linux = {
-          inky = inky;
-          default = inky;
+        extraInstallCommands = ''
+           mkdir -p "$out/share/applications"
+           ln -s ${desktopItem}/share/applications/* "$out/share/applications"
+         '';
+
+        meta = {
+          inherit description;
+          longDescription = ''
+            Inky is an editor for ink, inkle's markup language for writing interactive narrative in games, as used in "80 Days".
+            It's an IDE (integrated development environment), because it gives you a single app that lets you play in the editor as you write, and fix any bugs in your code.
+          '';
+          mainProgram = "inky";
+          homepage = "https://github.com/inkle/inky";
+          license = pkgs.lib.licenses.mit;
+          platforms = [ "x86_64-linux" ];
         };
       };
+
+    in {
+      inky = inky;
+      default = inky;
+    };
+  };
 }
