e872f55ebdfb8634761d4606d874ea8e94f24bdf
Michael Lange <dingoeatingfuzz@gmail.com>
2025-11-27 14:28:38 -0800

    25 for nixos

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,13 +2,13 @@
   description = "System";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     ghostty.url = "github:ghostty-org/ghostty";
     affinity.url = "github:mrshmllow/affinity-nix";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nvim-wrapper = {
