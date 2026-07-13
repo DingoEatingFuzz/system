8cb8d992df92fa9f7d9726141adb65a78dd4da80
Michael Lange <dingoeatingfuzz@gmail.com>
2026-07-13 16:31:34 -0700

    Experiment: move home-manager profiles into profiles directory

---
+++ profiles/wsl.nix
@@ -0,0 +1,100 @@
+{
+  config,
+  lib,
+  pkgs,
+  pkgs-unstable,
+  local,
+  system,
+  ...
+}:
+
+{
+  home.username = "nixos";
+
+  home.packages =
+    let
+      stable = with pkgs; [
+        neofetch
+        nurl
+
+        zip
+        xz
+        unzip
+        p7zip
+
+        ripgrep
+        jq
+        yq-go
+        eza
+        fzf
+        less
+        jless
+
+        which
+        tree
+        gawk
+
+        nix-output-monitor
+
+        chezmoi
+        starship
+        glow
+        btop
+        iotop
+        iftop
+
+        strace
+        ltrace
+        lsof
+
+        sysstat
+
+        git-credential-manager
+
+        nodejs
+        go
+
+        wsl-open
+        wslu
+
+        claude-code
+        _1password-cli
+      ];
+      unstable = with pkgs-unstable; [
+        (pkgs.writeShellScriptBin "nvim-old" "exec -a $0 ${neovim}/bin/nvim $@")
+      ];
+      custom = with local.packages.${system}; [
+        nvim
+        nomad
+      ];
+    in
+    stable ++ unstable ++ custom;
+
+  home.activation.chezmoi = lib.hm.dag.entryAfter [ "installPackages" ] ''
+    echo "Path? $PATH"
+    _path=$PATH
+    PATH="${config.home.path}/bin:$PATH"
+    echo "Setting up ChezMoi from ${config.home.homeDirectory}/system/dotfiles} ..."
+    ${pkgs.chezmoi}/bin/chezmoi apply --force --verbose -S ${config.home.homeDirectory}/system/dotfiles
+    PATH=$_path
+  '';
+
+  programs.git = {
+    enable = true;
+    userName = "Michael Lange";
+    userEmail = "dingoeatingfuzz@gmail.com";
+    extraConfig = {
+      credential.helper = "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe";
+      credential.credentialStore = "wincredman";
+      init.defaultBranch = "main";
+    };
+  };
+
+  programs.helix = import ./../lib/helix.nix {
+    helix = pkgs-unstable.helix;
+    pkgs = pkgs;
+  };
+
+  home.stateVersion = "24.05";
+  programs.home-manager.enable = true;
+}
