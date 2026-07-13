960935ec4d8eae3a95f4272b2b6ac2b48cdfeced
Michael Lange <dingoeatingfuzz@gmail.com>
2026-07-13 16:55:49 -0700

    use the alacritty lib config

--- nixos/home.nix
+++ nixos/home.nix
@@ -86,29 +86,8 @@
   };
 
   programs.alacritty = {
-    enable = true;
-    package = pkgs-unstable.alacritty;
-    settings = {
-      window.padding = {
-        x = 10;
-        y = 10;
-      };
-      scrolling = {
-        history = 5000;
-      };
-      font = {
-        size = 12;
-        # draw_bold_text_with_bright_colors = true;
-        normal = {
-          family = "SauceCodePro Nerd Font";
-          style = "Regular";
-        };
-        bold = {
-          family = "SauceCodePro Nerd Font";
-          style = "Bold";
-        };
-      };
-    };
+    alacritty = pkgs-unstable.alactritty;
+    pkgs = pkgs;
   };
 
   programs.helix = import ./../lib/helix.nix {
