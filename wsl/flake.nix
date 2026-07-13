c921dcc694c2b3b86089e94b933c4131408ca3b6
Michael Lange <dingoeatingfuzz@gmail.com>
2026-07-13 16:32:55 -0700

    Complete the machines/profiles refactor for wsl

--- wsl/flake.nix
+++ wsl/flake.nix
@@ -6,6 +6,9 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    ghostty.url = "github:ghostty-org/ghostty";
+    affinity.url = "github:mrshmllow/affinity-nix";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -18,32 +21,56 @@
 
   outputs =
     inputs@{ nixpkgs, home-manager, ... }:
+    let
+      unfree =
+        input: system:
+        import input {
+          inherit system;
+          config.allowUnfree = true;
+        };
+    in
     {
       nixosConfigurations = {
         wsl = nixpkgs.lib.nixosSystem rec {
           system = "x86_64-linux";
           specialArgs = {
             system = system;
-            pkgs-unstable = import inputs.nixpkgs-unstable {
-              inherit system;
-              config.allowUnfree = true;
-            };
+            pkgs-unstable = unfree inputs.nixpkgs-unstable system;
             local = inputs.local;
           };
           modules = [
             inputs.nixos-wsl.nixosModules.default
-            {
-              system.stateVersion = "25.05";
-              wsl.enable = true;
-            }
-            ./configuration.nix
-            ./fonts.nix
             home-manager.nixosModules.home-manager
+            ./../machines/wsl.nix
+            ./../lib/fonts.nix
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.extraSpecialArgs = specialArgs;
-              home-manager.users.nixos = import ./home.nix;
+              home-manager.users.nixos = import ./../profiles/wsl.nix;
+            }
+          ];
+        };
+        nixos = nixpkgs.lib.nixosSystem rec {
+          system = "x86_64-linux";
+          specialArgs = {
+            system = system;
+            pkgs-unstable = unfree inputs.nixpkgs-unstable system;
+            local = inputs.local;
+            ghostty = inputs.ghostty;
+            affinity = unfree inputs.affinity system;
+          };
+          modules = [
+            inputs.nixos-hardware.nixosModules.framework-intel-core-ultra-series1
+            home-manager.nixosModules.home-manager
+            ./configuration.nix
+            ./hardware-configuration.nix
+            ./fonts.nix
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = specialArgs;
+              home-manager.users.michael = import ./laptop-home.nix;
             }
           ];
         };
